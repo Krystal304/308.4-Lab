4285d0424a28fb3ce6c36979a0743e8b47afb74b
Krystal304 <krystalwilliams864@gmail.com>
2024-09-12 22:34:49 -0400

    update

--- script.js
+++ script.js
@@ -57,28 +57,83 @@
             // Working with other data
             // variable
     
-            let string ='Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
-                let cell = '';
-                let row = '';
-                        for(let i in string){
-                            switch(string[i]){
-                            case ',':
-                        row = row + cell + ' ';
-                         cell = ''
-                         break;
-                        case '\n':
-                            row = row + cell;
-                             cell = '';
-                        console.log(row)
-                         row = ''
-                        break;
-                    default:
-                        cell += string[i];
-                        break;
-                }
-                        if (i == string.length - 1){
-                             row += cell
-                        console.log(row)
-                }
-            }
+            // let string ='Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
+            //     let cell = '';
+            //     let row = '';
+            //             for(let i in string){
+            //                 switch(string[i]){
+            //                 case ',':
+            //             row = row + cell + ' ';
+            //              cell = ''
+            //              break;
+            //             case '\n':
+            //                 row = row + cell;
+            //                  cell = '';
+            //             console.log(row)
+            //              row = ''
+            //             break;
+            //         default:
+            //             cell += string[i];
+            //             break;
+            //     }
+            //             if (i == string.length - 1){
+            //                  row += cell
+            //             console.log(row)
+            //     }
+            // }
+            
+
+                    // Part 2: Expanding Functionality
+                    // Begin with the following task:
+                    // Declare a variable that stores the number of columns in each row of data within the CSV.
+                    // Instead of hard-coding four columns per row, expand your code to accept any number of columns.
+                    // This should be calculated dynamically based on the first row of data.
+
+                    // For example, if the first row of data (the headings) has eight entries, your program should create eight entries per row. 
+                    // You can safely assume that all rows that follow will contain the same number of entries per row.
+
+                    // After you have implemented the above:
+                    // Store your results in a two-dimensional array.
+                    // Each row should be its own array, with individual entries for each column.
+                    // Each row should be stored in a parent array, with the heading row located at index 0.
+                    // Cache this two-dimensional array in a variable for later use.
+
+            // Variable
+            // ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry
+            //  Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+
+            // let string = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
+
+            // let cell = ' ';
+
+            // let row = ' ';
+
+        //     let cell = '';
+        // let row = '';
+
+        // for(let i in string) {
+        //     switch (string[i]) {
+        //         case ',':
+        //             row = row + cell + ' ';
+        //             cell = ''
+        //             break;
+        //              case '\n':
+        //             row = row + cell;
+        //             cell = '';
+        //             console.log(row)
+        //                 row = ''
+                  
+        //             break;
+        //         default:
+        //             cell += string[i];
+        //             break;
+        //     }
+        //     if (i == string.length - 1){
+        //         row += cell
+        //         console.log(row)
+        //     }
+
+
+        // }
+           
             
