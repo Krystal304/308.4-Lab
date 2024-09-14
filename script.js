3a0f3f966287da4b9d9dac81bb485c29037f007c
Krystal304 <krystalwilliams864@gmail.com>
2024-09-13 21:13:07 -0400

    Part 3

--- script.js
+++ script.js
@@ -136,4 +136,53 @@
 
         // }
            
+
+
+                // part 3 Transforming data
+                // For each row of data in the result array produced by your code above,
+                //  create an object where the key of each value is the heading for that value’s column.
+                // Convert these keys to all lowercase letters for consistency.
+                // Store these objects in an array, in the order that they were originally listed.
+                // Since the heading for each column will be stored in the object keys, you do not need
+                // to create an object for the heading row itself.
+
+                let data = [
+                ["ID", "Name", "Occupation", "Age"],
+                ["42", "Bruce", "Knight", "41"],
+                ["57", "Bob", "Fry Cook", "19"],
+                ["63", "Blaine", "Quiz Master", "58"],
+                ["98", "Bill", "Doctor’s Assistant", "26"]
+        ];
+
+
+                // heading
+                let heading = data[0].map(h => h.toLowerCase());
+              
+                let rows = data.slice(1);
+
+                let array = []
+
+                rows.forEach(row=> {
+                        let obj = {};
+
+                heading.forEach((name, index, ) => {
+                        obj[name] = row[index] 
+                
+ 
+                        });
+                        array.push(obj);
+
+                });
+                console.log (array);
+
+
+                
+
+
+
+                
+
+
+
+
             
